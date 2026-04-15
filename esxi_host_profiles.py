86b4a2658e0bc26068bff0eceaf9098b79e30f1d
sarthaksatish <sarthaks5493@gmail.com>
2026-04-15 11:50:58 +0100

    Update esxi_host_profiles.py

--- esxi_host_profiles.py
+++ esxi_host_profiles.py
@@ -1,19 +1,18 @@
 index=ssc_inspec_event_idx exec_mode=weekly_cis_host_profile bunit=host_profile
 | spath
+| dedup host_name
 | table host_name overall_status platform hostprofiles ingestion_date
 
 index=ssc_inspec_event_idx exec_mode=weekly_cis_host_profile bunit=host_profile
 | spath
-| mvexpand results{}
-| spath input=results{}
-| table host_name overall_status platform hostprofiles ingestion_date status incompliancedescription
-
+| sort - ingestion_date
+| dedup host_name
+| table host_name overall_status platform hostprofiles ingestion_date
 
 index=ssc_inspec_event_idx exec_mode=weekly_cis_host_profile bunit=host_profile
 | spath
-| mvexpand results{}
-| spath input=results{} output=status path=status
-| spath input=results{} output=incompliancedescription path=incompliancedescription
+| mvexpand results
+| spath input=results
 | table host_name overall_status platform hostprofiles ingestion_date status incompliancedescription
 ---
 def get_overall_status(results, hostprofiles):
